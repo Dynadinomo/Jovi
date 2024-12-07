21cc7c8de17d6209ca3a972a3c273cf9ef9e9dd5
GaelSifuentes <144164170+Dynadinomo@users.noreply.github.com>
2024-12-06 23:31:55 -0600

    Delete My project directory

--- My project/Assets/Scenes/PlayerScenes/Scripts/TileScripts/TileController.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class TileController : MonoBehaviour
-{
-    public List<Vector3> tilePos = new List<Vector3>();
-}
